7fff891be69892dda35a34f347e05a3044ee320e
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2025-09-29 12:55:08 +0100

    ex xp day1

--- week_4/day_1/exercises/exercises_xp.js
+++ week_4/day_1/exercises/exercises_xp.js
@@ -1,1 +1,19 @@
-console.log("hello Abdelhay");
+// Résultat : "inside the funcOne function 3"
+// Affiche : 3
+// Avec const → Erreur (Assignment to constant variable)
+
+// Affiche : 0
+// Change a = 5
+// Affiche : 5
+// Avec const → Erreur (Assignment to constant variable)
+
+// Définit une variable globale a = "hello"
+// Affiche : "hello"
+
+// Résultat : "inside the funcSix function test"
+// Affiche : test
+// Avec const → Même résultat (pas d’erreur)
+
+// Résultat : "in the if block 5"
+// Résultat : "outside of the if block 2"
+// Avec const → Même résultat que let
